a02349ef686fe76155e4f62e7c56ac1fbc389482
coding-fan <daixiecode@qq.com>
2020-09-23 19:10:48 +0800

    Add gitlab and mongodb script

---
+++ gitlab/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# no need to install additional Database, quite convenient
+# First time, it will ask you to reset the password for admin user
+# then you can login with, root/new-password-you-set,
+# also you can register new users
+
+# It requires at least 2G memory, and consumes a large part of CPU
+# It may not OK to run inside VPS. Be careful!!
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+name=gitlab
+docker stop $name
+docker rm $name
+docker run -d --name $name \
+  -v ${SCRIPTPATH}/_data/opt:/var/opt/gitlab \
+  -v ${SCRIPTPATH}/_data/etc:/etc/gitlab \
+  -v ${SCRIPTPATH}/_data/log:/var/log/gitlab \
+  -p 8088:80 \
+  -p 3422:22 \
+  gitlab/gitlab-ce
+
+# after startup, access: http://localhost:8088/
---
+++ mongodb/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+name=mongo
+
+docker stop $name
+docker rm $name
+
+docker run -d --name $name -p 27017:27017 \
+  -v ${SCRIPTPATH}/_data:/data/db \
+  mongo:4.0
+# after startup, connect to it with dbeaver or other client
+# dburi: mongodb://localhost:27017/local
